6fbb7f1cfee70fdcd23b5fef247d5cd203c55faf
qnqs1324 <Anasstitou1324@gmail.com>
2023-05-01 13:49:44 +0200

    filelist aangepast

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ int main() {
     string c2="midi_files/";
     while(getline(Filelist,c)){
         MidiParser m(c2+c);
-        map<pair<unsigned int, bool>, set<Note*>> b = m.getNoteMap();
-        cout << c+ " " +to_string(b.size()) + " bytes"<< endl;
+        map<pair<unsigned int, bool>, set<Note*>> b = m.getNoteMap(); //TODO cascades_a_rag_(1904)_(nc)smythe.mid, morton_159s_mr_jelly_lord_(1924)_(nc)smythe.mid crashen het systeem,
+        cout << c+ " " +to_string(b.size()) + " bytes"<< endl; //TODO deze files zijn geschreven in rag time. kan zijn dat dit hierdoor komt, ik heb die voor nu uit de filelist gelaten.
     }
     Filelist.close();
     return 0;
