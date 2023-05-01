d6ba39dcdda59379848baf0f354d33855bccd958
qnqs1324 <Anasstitou1324@gmail.com>
2023-05-01 11:24:21 +0200

    MIDI FILES ADDED, lege classes added, README.MD voor MIDIFILES toegevoegd. deze README is niet compleet en moet nog uitgebreid worden als herzien

---
+++ src/ENFA.cpp
@@ -0,0 +1,5 @@
+//
+// Created by anass on 1-5-2023.
+//
+
+#include "ENFA.h"
---
+++ src/ENFA.h
@@ -0,0 +1,14 @@
+//
+// Created by anass on 1-5-2023.
+//
+
+#ifndef TA__TOG_ENFA_H
+#define TA__TOG_ENFA_H
+
+
+class ENFA {
+
+};
+
+
+#endif //TA__TOG_ENFA_H
