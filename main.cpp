9917e3dfb747fd69207ba8bfd279027ef62c6fe7
qnqs1324 <Anasstitou1324@gmail.com>
2023-05-01 13:28:53 +0200

    main doorloopt nu filelist waarin alle midifiles staan en parsed die, in de console wordt de midi file naam en erachter de bytes uitgedrukt

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include "midiparser/MidiParser.h"
+#include <algorithm>
 int main() {
-    MidiParser m("midi_files/t7.mid");
-    map<pair<unsigned int, bool>, set<Note*>> b = m.getNoteMap();
-    cout << b.size() << endl;
+    ifstream Filelist("filelist.txt");
+    string c;
+    string c2="midi_files/";
+    while(getline(Filelist,c)){
+        MidiParser m(c2+c);
+        map<pair<unsigned int, bool>, set<Note*>> b = m.getNoteMap();
+        cout << c+ " " +to_string(b.size()) + " bytes"<< endl;
+    }
+    Filelist.close();
     return 0;
 }
