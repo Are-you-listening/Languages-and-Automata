7ddd570027e42d957f7021171c93a128162e75b9
JeCheeseSmith <kars@duck.com>
2023-05-09 17:59:58 +0200

    Setup Genre

--- src/Genre.cpp
+++ src/Genre.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "Genre.h"
+
+double Genre::addGenre(const Song *&s) {
+    return 0;
+}
+
+vector<DFA> Genre::toProductAutomata() const {
+    return vector<DFA>();
+}
+
+Genre::Genre(const map<double, vector<Song *>> &members, double limit, const vector<bool> &param) : members(members),
+                                                                                                    limit(limit),
+                                                                                                    param(param) {}
+
+bool Genre::inGenre(const Song *&s) {
+    return false;
+}
+
+Genre::Genre(const Song *&s, const Song *&k, vector<bool> &param) {
+
+}
--- src/Genre.h
+++ src/Genre.h
@@ -5,10 +5,61 @@
 #ifndef TOG_GENRE_H
 #define TOG_GENRE_H
 
+#include <map>
+
 #include "Song.h"
+#include "DFA.h"
 
 class Genre {
+private:
+    /**
+     * \brief Contains the current members of this Genre sorted by its match %
+     */
+    map<double,vector<Song*>> members;
 
+    /**
+     * \brief The minimum % of Similarity a newly given Song has to have with the Genre to join it
+     */
+    double limit;
+
+    /**
+     * \brief The paramaters (for Similarity) this genre is build on
+     */
+    vector<bool> param;
+
+    /**
+     * \brief Generate a product automata for the current Genre's member-sub-regexes
+     * @return Datatype containing the sub DFA's in productautomata
+     */
+    vector<DFA> toProductAutomata() const;
+
+public:
+    /**
+     * \brief In case the match % <= limit, add to this Genre
+     * @param s
+     * @return match %
+     */
+    double addGenre(const Song* &s);
+
+    /**
+     * \brief Check if a given Song is part of this Genre
+     * @param s
+     * @return
+     */
+    bool inGenre(const Song* &s);
+
+    /**
+     * \brief Simple constructor
+     * @param members
+     * @param limit
+     * @param param
+     */
+    Genre(const map<double, vector<Song *>> &members, double limit, const vector<bool> &param);
+
+    /**
+     * \brief Create a Genre based on 2 Song's and a given set of paramaters.
+     */
+    Genre(const Song* &s, const Song* &k, vector<bool> &param);
 };
 
 
