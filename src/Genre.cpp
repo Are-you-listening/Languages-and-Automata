b02337fff35f4a2e6465a9c9b490da2a9a76a2ff
JeCheeseSmith <kars@duck.com>
2023-05-14 17:25:53 +0200

    Updateted Genre Constructor

--- src/Genre.cpp
+++ src/Genre.cpp
@@ -60,14 +60,24 @@ bool Genre::inGenre(Song *&s) {
 
 Genre::Genre(Song *&s, Song *&k, const vector<int> &params, const string &name) {
     param = params;
+    //REQUIRE PARAM LENGT
 
     //Set Data
     members={s,k};
-    ProductAutomata = {0, DFA() };
     this->name = name;
     fInitCheck = this;
 
-    ProductAutomata = {2,toProductAutomata()}; //Construct First ProductAutomata
+    //Create Seperate DFA's
+    vector<RE> t = members[0]->toRegex(param[0],param[1],param[2],param[3],param[4],-1); //Set pattern to -1 so we can generate 1 big Regex
+    ENFA a = t[0].toENFA();
+    DFA z = a.toDFA();
+
+    //Other DFA
+    vector<RE> t2 = members[1]->toRegex(param[0],param[1],param[2],param[3],param[4],-1); //Set pattern to -1 so we can generate 1 big Regex
+    ENFA a2 = t2[0].toENFA();
+    DFA z2 = a.toDFA();
+
+    ProductAutomata = {2,DFA(z,z2,0)}; //Construct First ProductAutomata
 
     string log = getCurrTime() + " Created the new Genre: "+name+" , based on "+ s->getTitle() + " and " + k->getTitle() +"\n\n";
     if(console){cout << log;}
