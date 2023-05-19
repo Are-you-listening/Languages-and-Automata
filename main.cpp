1b8bb98ce030f9abe578c5551709718be9471489
tibov <tibo.verreycken@telenet.be>
2023-05-19 20:40:14 +0200

    song genrate fixed

--- main.cpp
+++ main.cpp
@@ -8,7 +8,65 @@
 #include "Genre.h"
 
 #include "NFA.h"
+#include "gui/Gui.h"
 int main() {
+
+    /*
+    Gui g;
+    g.start();
+     */
+    /*
+    Song* s = new Song("midi_files/HarryPotter.mid", 0);
+    Song* s2 = new Song("midi_files/HarryPotter.mid", 0);
+     s->similarity(*s2, 0, 0);
+     */
+
+    /*
+    Song s("midi_files/c0.mid" , true);
+    Song k("midi_files/c1.mid" , true);
+    s.similarity(k,false, false);
+    cout << ":";*/
+
+    Song* s = new Song("midi_files/HarryPotter.mid", 0);
+    Song* s2 = new Song("midi_files/HarryPotter.mid", 0);
+
+    //vector<int> V={1,1,1,1,0,-1};
+    //Genre genre = Genre(s,s2,V,"_compare_",0, 0);
+    //DFA genreDFA=genre.getProductAutomata();
+    RE r = s->toRegex(1, 1, 1, 1, 1 , -1)[0];
+    ENFA e = r.toENFA();
+    DFA d = e.toDFA();
+    vector<int> v = {1,1,1,1,1,-1};
+    Song* s3 = new Song(d, v, 0);
+    s3->save("out.mid");
+    Song* s4 = new Song("out.mid", 0);
+    s4->save("out.mid");
+
+
+    /*
+    Song* s = new Song("midi_files/HarryPotter.mid", 0);
+    Song* s2 = new Song("midi_files/t2.mid", 0);
+    RE r = s->toRegex(0, 0, 0, 1, 1, -1)[0];
+    RE r3 = s2->toRegex(0, 0, 0, 1, 1, -1)[0];
+    RE r2 = s->toRegex(0, 0, 0, 1, 1, -1)[0];
+    ENFA e = r.toENFA();
+    ENFA e3 = r3.toENFA();
+    DFA d = e.toDFA();
+    DFA d3 = e3.toDFA();
+
+
+    vector<int> V={0,0,0,1,1,-1};
+    cout << d3.getStates().size() << " " << d.getStates().size() << endl;
+    cout << d.minimize().getStates().size() << endl;
+    cout << "b" << endl;
+    Genre genre = Genre(s,s2,V,"_compare_",0);
+    DFA genreDFA=genre.getProductAutomata();
+    cout << genreDFA.accepts(r2.re) << endl;
+    cout << genreDFA.getStates().size() << endl;
+     */
+    //cout << d2.getStates().size() << endl;
+    //cout << d2.accepts(r2.re) << endl;
+    /*
     vector<pair<string,string>> doubleComparison; // TODO enfa alpahbet bezit soms over incomplete sequence
     ifstream Filelist("filelist2nd.txt"); 
     string c;
@@ -28,18 +86,17 @@ int main() {
             Song* song2 = new Song(c2+c3,0);
             song2->setTitle(c3);
             vector<int> V={1,1,1,1,0,1};
-            /*
             Genre genre = Genre(song,song2,V,c+"_compare_"+c3,1);
             DFA genreDFA=genre.getProductAutomata();
             Song generated=Song(genreDFA,V,1);
             string path="midi_output/"+c+"_compare_"+c3;
             generated.save(path);
-             */
             song->similarity(*song2,0,0);
             delete song2;
         }
         delete song;
     }
     Filelist.close();
+     */
     return 0;
 }
--- src/Song.cpp
+++ src/Song.cpp
@@ -87,7 +87,7 @@ Song::Song(const string &path, bool console) : console(console) {
     REQUIRE(FileExists(path) , "Given file not found");
 
     fInitCheck=this;
-    title = path.substr(11, path.size()-10);
+    //title = path.substr(11, 4);
 
     string log = getCurrTime() + " Parsing note_map from .mid file...\n\n";
     if(console){cout << log;}
