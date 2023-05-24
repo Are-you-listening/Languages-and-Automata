f734ec4d13f4b91d2f69303eb8cff24744d2cdf5
qnqs1324 <Anasstitou1324@gmail.com>
2023-05-24 14:21:26 +0200

    GROUP CHANGES

--- main.cpp
+++ main.cpp
@@ -8,15 +8,16 @@
 
 int main() {
     vector<pair<string,string>> doubleComparison;
-    ifstream Filelist("filelistdemo.txt"); // hier moet je filelist met je naam komen
+    ifstream Filelist("filelistAnas.txt"); // hier moet je filelist met je naam komen
     ofstream errorfile("errors.txt");
     string c;
     string c2="midi_files/";
+    vector<int> V = {1, 1, 1, 1, 1, -1};
     vector<Song*> songs;
     while(getline(Filelist,c)){
         Song* song = new Song(c2+c,true);
         song->setTitle(c);
-        ifstream Filelist2("filelistdemo.txt"); // dit ongewijzigd laten a.u.b.
+        ifstream Filelist2("filelist3.txt"); // dit ongewijzigd laten a.u.b.
         string c3;
         while(getline(Filelist2,c3)){
             if(find(doubleComparison.begin(), doubleComparison.end(), make_pair(c,c3))!=doubleComparison.end()){
@@ -27,7 +28,6 @@ int main() {
             Song* song2 = new Song(c2+c3,false);
             song2->setTitle(c3);
             try {
-                vector<int> V = {1, 1, 1, 1, 1, -1};
                 Genre genre = Genre(song, song2, V, c + "_compare_" + c3, 0.70, 1, 0);
                 DFA *genreDFA = genre.getProductAutomata();
                 Song generated = Song(genreDFA, V, true);
